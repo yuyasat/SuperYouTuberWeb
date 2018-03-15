e60a85534b0e8c4d0e0e4bdfe31b92ecde047fc5
yuyasat <takiyour@gmail.com>
2018-03-16 00:56:29 +0900

    set title

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,6 @@ module AppVotematch4
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
   end
 end
