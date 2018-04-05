e8542f3692f051ea9c1612b38caeb0e8313603b6
yuyasat <takiyour@gmail.com>
2018-04-06 01:02:32 +0900

    enable postgis extension

--- db/migrate/20180318000000_create_locations.rb
+++ db/migrate/20180318000000_create_locations.rb
@@ -1,5 +1,7 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
+    enable_extension 'postgis'
+
     create_table :locations do |t|
       t.references :movie, null: false
       t.st_point :lonlat, geographic: true, null: false
