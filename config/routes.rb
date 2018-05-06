66f79ae6a347f5e60e8eedc23a0372327d1366f0
yuyasat <takiyour@gmail.com>
2018-05-06 14:11:06 +0900

    add movies show page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :movies, only: %i(show)
+
   resources :video_artists, path: 'youtubers', only: %i(index show)
 
   resources :music, only: %i(index show) do
