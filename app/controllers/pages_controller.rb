46ebb2f0deea70b4bec2ff41904fc2d516e38538
minojiro <lo@minojiro.com>
2018-05-07 08:36:59 +0900

    design small fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     @featured_movie = Movie.joins(:featured_movies).merge(FeaturedMovie.active).sample
     @latest_movies = Movie.order(published_at: :desc).limit(10)
     @category_movies = Movie.grouped_by_categories(num: 8)
+    @mobile_menu_show = true;
   end
 
   def privacy_policy
