92f380b7784134b60101fca1877a93d9232840f9
yuyasat <takiyour@gmail.com>
2018-12-01 12:22:27 +0900

    add page to category_type

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -5,10 +5,11 @@ module Types
     field :display_order, Integer, null: false
     field :movies, [Types::MovieType], null: true do
       argument :num, Integer, required: false
+      argument :page, Integer, required: false
     end
 
-    def movies(num: 8)
-      object.related_categories_movies.limit(num)
+    def movies(num: 8, page: 1)
+      object.related_categories_movies.page(page).per(8)
     end
   end
 end
